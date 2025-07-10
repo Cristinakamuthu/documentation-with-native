bef74790aa9c410bfad4bc9add5226b1dbb3b6ef
Cristina <kamuthucristina@gmail.com>
2025-07-10 22:10:21 +0300

    first home route

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,23 @@
-import { Text, View } from "react-native";
-
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+import { StyleSheet, Text, View } from "react-native"
+export default function Homescreen(){
+  return(
+    <View style = {styles.container}>
+      <Text style={styles.text}>Home</Text>
     </View>
-  );
+  )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent:"center",
+    alignItems:'center',
+    
+    backgroundColor :'#000'
+  },
+  text:{
+    color:'white',
+    fontSize:56,
+  }
+
+})
